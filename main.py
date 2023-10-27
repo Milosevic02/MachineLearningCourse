ea1d730eaf7fab970878ac0c50f4d887dc6a0f13
Dragan <milosevicdragan002@gmail.com>
2023-10-27 18:49:07 +0200

    Create OneHot encoder

--- main.py
+++ main.py
@@ -21,3 +21,15 @@ null_value_stats = X.isnull().sum(axis=0)
 null_value_stats[null_value_stats != 0]
 
 X = X.fillna('Unknown') 
+
+from sklearn.model_selection import train_test_split
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.75, random_state=42)
+
+import category_encoders as ce
+
+ohe = ce.OneHotEncoder(handle_unknown='value', use_cat_names=True)
+X_train_ohe = ohe.fit_transform(X_train)
+X_train_ohe.sample(5)
+
+X_valid_ohe = ohe.transform(X_valid)
+X_valid_ohe.sample(5)
